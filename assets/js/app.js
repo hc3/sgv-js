3de34723dad40c3fb5da88a5107f6b1394f96c1f
Eliel <eliel.floyd@bol.com.br>
2016-05-28 07:33:13 -0300

    ADICIONADO O MENU LUBRIFICACAO

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-angular.module("dashboard",['ngRoute'])
+angular.module("dashboard",['ngRoute','angular.morris-chart'])
 	
 	.config(function($routeProvider) {
 		$routeProvider
@@ -11,11 +11,38 @@ angular.module("dashboard",['ngRoute'])
 				templateUrl:"views/listVeiculo.html",
 				controller:"VeiculoController",
 				controllerAs:"VeiculoCtrl"
+			})
+			.when('/lubriCad',{
+				templateUrl:"views/cadLubri.html",
+				controller:"LubrificacaoController",
+				controllerAs:"LubriCtrl"
+			})
+			.when('/lubriList',{
+				templateUrl:"views/listLubri.html",
+				controller:"LubrificacaoController",
+				controllerAs:"LubriCtrl"
+			})
+			.when('/graficos',{
+				templateUrl:"views/graficos.html",
+				controller:"LubrificacaoController",
+				controllerAs:"LubriCtrl"				
 			});
 	})
 
-	.controller("VeiculoController",VeiculoController);
+	.controller("VeiculoController",VeiculoController)
+	.controller("LubrificacaoController",LubrificacaoController);
 
+function LubrificacaoController() {
+	var vm = this;
+	vm.lubrificacoes = [
+		{veiculo:"JOÃO",custoKm:"0,55",mediaKm:"5,96",data:"JANEIRO"},
+		{veiculo:"ASTROGILDO",custoKm:"0,60",mediaKm:"5,80",data:"JANEIRO"},
+		{veiculo:"RUBÊNCIO",custoKm:"0,57",mediaKm:"5,60",data:"JANEIRO"},
+		{veiculo:"ALEXSANDRO",custoKm:"0,58",mediaKm:"9,52",data:"JANEIRO"},
+		{veiculo:"MONTIVALDO",custoKm:"0,59",mediaKm:"3,54",data:"JANEIRO"},
+		{veiculo:"PAULO",custoKm:"0,54",mediaKm:"5,8",data:"JANEIRO"},
+	]
+}
 
 function VeiculoController() {
 	var vm = this;
@@ -33,6 +60,7 @@ function VeiculoController() {
 	function add(veiculo) {
 		vm.veiculos.push(angular.copy(veiculo));
 		vm.form = {};
+		console.log(vm.veiculos);
 	}
 
 	vm.limpar = limpar;

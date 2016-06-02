49272d74920224bffa3f1cb04854e138b451029a
Eliel <eliel.floyd@bol.com.br>
2016-06-02 10:27:52 -0300

    adicionado os services para lubrificacao e veiculo ainda nao sei se vou implementar uma factory ou um service

---
+++ public/assets/js/service/lubrificacaoService.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+
+	angular.module('dashboard')
+		.service('LubrificacaoService', LubrificacaoService)
+
+function LubrificacaoService() {
+
+}
+
+})();
---
+++ public/assets/js/service/veiculoService.js
@@ -0,0 +1,11 @@
+(function() {
+	'use strict';
+
+	angular.module('dashboad')
+		.service('VeiculoService', VeiculoService);
+
+function VeiculoService() {
+
+}
+
+})();
